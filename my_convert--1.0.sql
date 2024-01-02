ea5ce5a617a4594ca1509053c8e1a219fdd65e29
antuanviolin <89915010+antuanviolin@users.noreply.github.com>
2024-01-02 17:59:33 +0700

    Update my_convert--1.0.sql

--- my_convert--1.0.sql
+++ my_convert--1.0.sql
@@ -7,4 +7,13 @@ $BODY$
 $BODY$
 LANGUAGE 'sql' IMMUTABLE;
 
+CREATE OR REPLACE FUNCTION json_to_hstore(j0 json)
+RETURNS hstore AS 
+$BODY$
+    SELECT hstore(array_agg(key), array_agg(value))
+    FROM json_each_text(j0)
+$BODY$
+LANGUAGE 'sql' IMMUTABLE;
+
 CREATE CAST (jsonb AS hstore) WITH FUNCTION jsonb_to_hstore(jsonb) AS IMPLICIT;
+CREATE CAST (json AS hstore) WITH FUNCTION json_to_hstore(json) AS IMPLICIT;
