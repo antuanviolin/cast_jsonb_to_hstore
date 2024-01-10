964945fd8fb7a9cbc8edd8ecb824926e82f71516
antuanviolin <89915010+antuanviolin@users.noreply.github.com>
2024-01-10 21:09:47 +0700

    new version

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
+MODULES = my_convert
 EXTENSION = my_convert
 DATA = my_convert--1.0.sql
 PGFILEDESC = "Convert data between different character sets"
 REGRESS = my_convert
+EXTRA_INSTALL = contrib/hstore
 
 ifdef USE_PGXS
 PG_CONFIG = PG_CONFIG
 PGXS := $(shell $(PG_CONFIG) --pgxs)
 include $(PGXS)
 else
+PG_CPPFLAGS = -I$(top_srcdir)/contrib
 subdir = contrib/my_convert
 top_builddir = ../..
 include $(top_builddir)/src/Makefile.global
--- my_convert--1.0.sql
+++ my_convert--1.0.sql
@@ -1,11 +1,8 @@
 \echo Use "CREATE EXTENSION my_convert" to load this file. \quit
 CREATE OR REPLACE FUNCTION jsonb_to_hstore(j0 jsonb)
-RETURNS hstore AS 
-$BODY$
-    SELECT hstore(array_agg(key), array_agg(value))
-    FROM jsonb_each_text(j0)
-$BODY$
-LANGUAGE 'sql' IMMUTABLE;
+RETURNS hstore 
+AS '$libdir/my_convert', 'jsonb_to_hstore'
+LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION json_to_hstore(j0 json)
 RETURNS hstore AS 
